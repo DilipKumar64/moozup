fee71dce8f28d5cdb3c29272cd34ac1f7c6f5524
deepmali890 <deepmali590@gmail.com>
2025-05-15 15:24:43 +0530

    Added authLimiter middleware to all routes in auth.route.js

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -6,8 +6,8 @@ const userController = require('../controllers/auth.controller');
 const router = express.Router();
 
 // Signup and login routes
-router.post('/signup', userController.signup);
-router.post('/login', userController.login);
+router.post('/signup',authLimiter , userController.signup);
+router.post('/login', authLimiter, userController.login);
 
 // Logout route
 router.post('/logout', userController.logout); // Using the logout method from userController
@@ -17,13 +17,13 @@ router.post("/refresh-token",authLimiter, userController.refreshToken);
 
 // Password reset route (protected)
 // 1. Request OTP (email or phone se)
-router.post('/request-reset-otp', userController.requestResetOtp);
+router.post('/request-reset-otp',authLimiter, userController.requestResetOtp);
 
 // 2. Verify OTP
-router.post('/verify-reset-otp', userController.verifyResetOtp);
+router.post('/verify-reset-otp',authLimiter, userController.verifyResetOtp);
 
 // 3. Reset password after OTP verification
-router.post('/reset-password', userController.resetPassword);
+router.post('/reset-password',authLimiter, userController.resetPassword);
 
 module.exports = router;
  
